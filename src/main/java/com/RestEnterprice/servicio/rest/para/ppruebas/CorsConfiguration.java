de1ea973386b1114162046575a858e73971494ca
christopher-cuadros <110701105+visual-junior@users.noreply.github.com>
2023-10-28 21:24:43 -0500

    prueba

--- src/main/java/com/RestEnterprice/servicio/rest/para/ppruebas/CorsConfiguration.java
+++ src/main/java/com/RestEnterprice/servicio/rest/para/ppruebas/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200,http://127.0.0.7:6500")
+                .allowedOrigins("**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
                 
